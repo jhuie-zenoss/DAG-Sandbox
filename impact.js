8203036ca3067d2cc89116cfbab5dd4fdab625b3
John Crocker <jcrocker@zenoss.com>
2013-10-20 20:37:50 -0500

    Added on-click to hide children

--- impact.js
+++ impact.js
@@ -9,7 +9,7 @@ function ImpactGraph(root, width, height, dataLoc){
 	var imgURL = "http://192.168.0.4:8080";
 	var dataLoc = dataLoc;
 	var g = new dagreD3.Digraph();
-    var duration = 1500;
+    var duration = 500;
 	var renderer = new dagreD3.Renderer();
     //set new draw method
     oldDrawNode = renderer.drawNode();
@@ -20,7 +20,7 @@ function ImpactGraph(root, width, height, dataLoc){
 
     renderer.createNodes(function (graph, svg) {
         var nodes = svg.selectAll("g .node")
-            .data(graph.nodes());
+            .data(graph.nodes(), function(d, i) { return d; });
         var enter = nodes.enter()
             .append("g")
             .style("opacity", 1e-6)
@@ -28,12 +28,16 @@ function ImpactGraph(root, width, height, dataLoc){
                 var n = graph.node(d);
                 return "translate(" + n.y0 + "," + n.x0 + ")";
             })
+            .on("click", onClick)
             .classed("node", true);
 
         var update = nodes
             .transition()
             .duration(duration)
             .style("opacity", 1);
+
+        var exit = nodes.exit()
+            .remove();
         return update;
 
     });
@@ -41,10 +45,9 @@ function ImpactGraph(root, width, height, dataLoc){
     renderer.createEdges(function(graph, svg) {
         var edges = svg
             .selectAll("g .edge")
-            .data(graph.edges());
+            .data(graph.edges(), function(d) {return d;});
 
-        var enter = edges
-            .enter()
+        var enter = edges.enter()
             .insert("g", "*")
             .classed("edge", true)
             .attr("transform", "translate (" + width/2 + "," + height/2 + ") scale(0,0)")
@@ -55,6 +58,9 @@ function ImpactGraph(root, width, height, dataLoc){
             .duration(duration)
             .attr("transform", "scale(1,1)")
             .style("stroke-opacity", 1);
+
+        var exit = edges.exit()
+            .remove();
 
         return enter;
     } );
@@ -81,7 +87,8 @@ function ImpactGraph(root, width, height, dataLoc){
 		    }
 			
 			for(var i=0; i < edges.length; ++i){
-				g.addEdge(edges[i].id, edges[i].to, edges[i].from);
+                var id = edges[i].from + "_" + edges[i].to;
+                g.addEdge(id, edges[i].to, edges[i].from);
 		    }
 		    
 		    drawGraph();
@@ -89,8 +96,6 @@ function ImpactGraph(root, width, height, dataLoc){
 	}
 	
 	function drawGraph(){
-
-		
 	    // Cleanup old graph
 	    var svg = d3.select("svg");   
 	    renderer.layout(layout).run(g, svg.select("g"));
@@ -101,4 +106,17 @@ function ImpactGraph(root, width, height, dataLoc){
 		      .attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
 		 }));
 	}
+
+    function onClick(d) {
+        console.log(d);
+        var children = g.successors(d);
+        for (var i=0; i<children.length;++i) {
+            g.delNode(children[i]);
+        }
+        var edges = g.outEdges(d);
+        for (var i=0; i < edges.length;++i) {
+            g.delEdge(edges[i]);
+        }
+        drawGraph();
+    }
 }
